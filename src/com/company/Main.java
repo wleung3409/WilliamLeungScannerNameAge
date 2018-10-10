9516132739148a9e178aede5ffb0728ef294a4bb
William <wleung3409@bths.edu>
2018-10-09 20:37:58 -0400

    Commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,13 +4,14 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args,int age){
+    public static void main(String[] args){
 	// write your code here
         Scanner input= new Scanner(System.in);
         System.out.println("What is your name?");
         String name=input.nextLine();
         System.out.println("Hello "+name+", nice to meet you");
         System.out.println("How old are you?");
-        int age=input.nextInt();
-        }
+        int ree=input.nextInt();
+        System.out.println("You are "+ree+" years old");
+    }
 }
